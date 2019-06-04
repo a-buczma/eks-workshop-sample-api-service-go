9ad6fa94ff13b2efe0eba4aef86e2ba0acf206c5
a-buczma <49633449+a-buczma@users.noreply.github.com>
2019-06-04 15:13:39 +0200

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello World - ele mele dutki"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
